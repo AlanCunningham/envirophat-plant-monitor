14c633731c7886883a8a8ed77f6be4f4dfe37d44
Alan Cunningham <ajc.alan@gmail.com>
2020-05-20 22:14:48 +0100

    Set the datetime field in SensorData to auto_add_now
    Also set a default string for SensorData items

--- monitor/models.py
+++ monitor/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils.dateformat import format
 
 class SensorData(models.Model):
-    datetime = models.DateTimeField(auto_now=True)
+
+    datetime = models.DateTimeField(auto_now_add=True)
     temperature = models.IntegerField()
     moisture = models.DecimalField(max_digits=2, decimal_places=2)
     light = models.DecimalField(max_digits=2, decimal_places=2)
+
+    def __str__(self):
+        return format(self.datetime, settings.DATETIME_FORMAT)
